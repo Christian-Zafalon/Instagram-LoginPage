94a6b23a1d3bcf4d3c6ee3d55b3419303c16adbd
Christian Zafalon <chris.zafa.2013@gmail.com>
2022-06-29 17:46:29 -0300

    PrimeiroCommit

---
+++ src/app/insta-component/insta-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insta-component',
+  templateUrl: './insta-component.component.html',
+  styleUrls: ['./insta-component.component.css']
+})
+export class InstaComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
